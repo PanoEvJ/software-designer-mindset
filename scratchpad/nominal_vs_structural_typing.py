c23c729e0a672e7597f3d3a77ec5a87737cc5646
Pano <panosevag@gmail.com>
2023-12-23 11:47:12 +0200

    initialized book class

---
+++ scratchpad/nominal_vs_structural_typing.py
@@ -0,0 +1,13 @@
+class Book:
+    def __init__(self, title: str, author: str, pages: int) -> None:
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+
+def main() -> None:
+    var = 1
+
+
+if "__main__" == __name__:
+    main()
