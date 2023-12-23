3bb69e8682bf7aa91094047736b7454e7f2fbb45
Pano <panosevag@gmail.com>
2023-12-23 13:36:58 +0200

    TUPLES

--- scratchpad/enums.py
+++ scratchpad/enums.py
@@ -20,9 +20,15 @@ def is_birthday(month: Month):
     return month == Month.JAN
 
 
+def birthday_month_year() -> tuple[Month, int]:
+    return Month.JAN, 1988
+
+
 def main() -> None:
     print(f"Is January my birthday month? {is_birthday(Month.JAN)}")
-    print(Month.JAN.value)
+    print(birthday_month_year())
+    my_month, my_year = birthday_month_year()
+    print(f"I was born in {my_month.value} of {my_year}")
 
 
 if "__main__" == __name__:
