090a7a3df2dfd210cc70d51fb162413575cc2d70
Pano <panosevag@gmail.com>
2023-12-23 21:42:09 +0200

    dataclasses training

--- mastering_the_tools/classes_and_dataclasses/before.py
+++ mastering_the_tools/classes_and_dataclasses/before.py
@@ -1,5 +1,6 @@
 import random
 import string
+from dataclasses import dataclass, field
 from enum import Enum, auto
 
 
@@ -27,22 +28,16 @@ class FuelType(Enum):
     ELECTRIC = auto()
 
 
+@dataclass(frozen=True)
 class Vehicle:
-    def __init__(
-        self,
-        brand: str,
-        model: str,
-        color: str,
-        license_plate: str,
-        fuel_type: FuelType = FuelType.ELECTRIC,
-        accessories: list[Accessory] = [],
-    ) -> None:
-        self.brand = brand
-        self.model = model
-        self.color = color
-        self.fuel_type = fuel_type
-        self.license_plate = license_plate
-        self.accessories = accessories
+    brand: str
+    model: str
+    color: str
+    license_plate: str = field(default_factory=generate_vehicle_license)
+    fuel_type: FuelType = FuelType.ELECTRIC
+    accessories: list[Accessory] = field(
+        default_factory=lambda: [Accessory.AIRCO, Accessory.NAVIGATION]
+    )
 
 
 def main() -> None:
@@ -54,20 +49,13 @@ def main() -> None:
         brand="Tesla",
         model="Model 3",
         color="black",
-        license_plate=generate_vehicle_license(),
-        accessories=[
-            Accessory.AIRCO,
-            Accessory.MINIBAR,
-            Accessory.NAVIGATION,
-            Accessory.CRUISECONTROL,
-        ],
     )
     volkswagen = Vehicle(
         brand="Volkswagen",
         model="ID3",
         color="white",
-        license_plate=generate_vehicle_license(),
-        accessories=[Accessory.AIRCO, Accessory.NAVIGATION],
+        fuel_type=FuelType.DIESEL,
+        accessories=[Accessory.NAVIGATION, Accessory.CRUISECONTROL],
     )
     bmw = Vehicle(
         brand="BMW",
@@ -75,7 +63,7 @@ def main() -> None:
         color="blue",
         license_plate=generate_vehicle_license(),
         fuel_type=FuelType.PETROL,
-        accessories=[Accessory.NAVIGATION, Accessory.CRUISECONTROL],
+        accessories=[Accessory.OPENROOF],
     )
 
     print(tesla)
