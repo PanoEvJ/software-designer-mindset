5d87fecafce9ad3dfd550292d5ce19217fd6cbbd
Pano <panosevag@gmail.com>
2023-12-23 12:09:57 +0200

    nominal vs structural typing

--- scratchpad/nominal_vs_structural_typing.py
+++ scratchpad/nominal_vs_structural_typing.py
@@ -4,9 +4,19 @@ class Book:
         self.author = author
         self.pages = pages
 
+    def __len__(self) -> int:
+        return self.pages
+
 
 def main() -> None:
-    var = 1
+    my_str = "Hello"
+    print(len(my_str))
+    my_list = [1, 2, 3]
+    print(len(my_list))
+    my_dict = {"a": 1, "b": 2}
+    print(len(my_dict))
+    my_book = Book("War and Peace", "Leo Tolstoy", 1225)
+    print(len(my_book))
 
 
 if "__main__" == __name__:
