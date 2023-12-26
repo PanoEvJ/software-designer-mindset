d4c9ef660d27dfa507b40dcc63076a3674bc036e
Pano <panosevag@gmail.com>
2023-12-26 12:40:01 +0200

    first iteration for composition with protocol

--- the_7_principles_of_modern_software_design/favor_composition_over_inheritance/employee_example/my_with_composition.py
+++ the_7_principles_of_modern_software_design/favor_composition_over_inheritance/employee_example/my_with_composition.py
@@ -3,69 +3,72 @@ Very advanced Employee management system.
 """
 
 from dataclasses import dataclass
+from typing import Protocol
+
+
+class PaymentSources(Protocol):
+    def compute_pay(self) -> int:
+        ...
 
 
 @dataclass
-class Commission:
+class HourlyPayment:
+    pay_rate: int
+    hours_worked: float = 0
+    employer_cost: int = 100000
+
+    def compute_pay(
+        self,
+    ) -> int:
+        return int(self.pay_rate * self.hours_worked + self.employer_cost)
+
+
+@dataclass
+class SalaryPayment:
+    monthly_salary: int
+    percentage: float = 1
+
+    def compute_pay(
+        self,
+    ) -> int:
+        return int(self.monthly_salary * self.percentage)
+
+
+@dataclass
+class CommissionPayment:
     commission: int = 10000
     contracts_landed: float = 0
 
-    def compute_commission_payment(self) -> int:
+    def compute_pay(self) -> int:
         return int(self.commission * self.contracts_landed)
 
 
 @dataclass
-class HourlyEmployee:
+class Employee:
     name: str
     id: int
-    pay_rate: int = 0
-    hours_worked: float = 0
-    employer_cost: int = 100000
 
-    def compute_pay(self, commission: Commission) -> int:
-        total = int(self.pay_rate * self.hours_worked + self.employer_cost)
-        if commission:
-            total += commission.compute_commission_payment()
-        return total
-
-
-@dataclass
-class SalariedEmployee:
-    name: str
-    id: int
-    monthly_salary: int = 0
-    percentage: float = 1
-
-    def compute_pay(self, commission: Commission) -> int:
-        total = int(self.monthly_salary * self.percentage)
-        if commission:
-            total += commission.compute_commission_payment()
-        return total
-
-
-@dataclass
-class Freelancer:
-    name: str
-    id: int
-    pay_rate: int = 0
-    hours_worked: float = 0
-    vat_number: str = ""
-
-    def compute_pay(self, commission: Commission) -> int:
-        total = int(self.pay_rate * self.hours_worked)
-        if commission:
-            total += commission.compute_commission_payment()
+    def compute_pay(self, payment_sources: list[PaymentSources]) -> int:
+        total: int = 0
+        for source in payment_sources:
+            total += source.compute_pay()
         return total
 
 
 def main() -> None:
-    henry = HourlyEmployee(name="Henry", id=12346, pay_rate=5000, hours_worked=100)
-    henry_commission = Commission(contracts_landed=5)
-    print(f"{henry.name} earned ${(henry.compute_pay(henry_commission) / 100):.2f}.")
+    henry = Employee(name="Henry", id=12346)
+    henry_hourly_payment = HourlyPayment(pay_rate=5000, hours_worked=100)
+    henry_commission_payment = CommissionPayment(contracts_landed=5)
+    print(
+        f"{henry.name} earned ${(henry.compute_pay([henry_hourly_payment, henry_commission_payment]) / 100):.2f}."
+    )
 
-    sarah = SalariedEmployee(name="Sarah", id=47832, monthly_salary=5000000)
-    sarah_commission = Commission(contracts_landed=10)
-    print(f"{sarah.name} earned ${(sarah.compute_pay(sarah_commission) / 100):.2f}.")
+    sarah = Employee(name="Sarah", id=47832)
+    sarah_salary_payment = SalaryPayment(monthly_salary=5000000)
+    sarah_commission_payment = CommissionPayment(contracts_landed=10)
+    print(
+        f"{sarah.name} earned ${(sarah.compute_pay([sarah_salary_payment, sarah_commission_payment]) / 100):.2f}."
+    )
 
 
 if __name__ == "__main__":
