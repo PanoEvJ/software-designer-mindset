004972a8ff847c3cb539c71060f0789ed289732e
Pano <panosevag@gmail.com>
2023-12-27 11:51:55 +0200

    completed cohesion ecercise

--- the_7_principles_of_modern_software_design/high_cohesion/exercises/exercise.py
+++ the_7_principles_of_modern_software_design/high_cohesion/exercises/exercise.py
@@ -3,25 +3,39 @@ from typing import Any
 
 Record = dict[str, Any]
 
+INPUT_FILE = "data.csv"
+OUTPUT_FILE = "processed.csv"
+FIELD_NAMES_OUTPUT = ["name", "status", "is_active"]
+
+
+def from_csv(filename: str) -> list[Record]:
+    with open(filename, mode="r", encoding="csv") as f:
+        reader = csv.DictReader(f)
+        return list(reader)
+
+
+def to_csv(data: list[Record], filename: str, fieldnames: list[str]) -> None:
+    with open(filename, mode="w", encoding="csv") as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(data)
+
+
+def process_data(row: Record) -> Record:
+    row_copy = row.copy()
+    if row_copy["status"] == "active":
+        row_copy["is_active"] = True
+    else:
+        row_copy["is_active"] = False
+    return row_copy
+
 
 def main() -> None:
-    with open("data.csv") as f:
-        reader = csv.DictReader(f)
-        data: list[Record] = list(reader)
+    data = from_csv("data.csv")
 
-        processed_data: list[Record] = []
-        for row in data:
-            row_copy = row.copy()
-            if row_copy["status"] == "active":
-                row_copy["is_active"] = True
-            else:
-                row_copy["is_active"] = False
-            processed_data.append(row_copy)
+    processed_data = [process_data(row) for row in data]
 
-        with open("processed.csv", "w") as f:
-            writer = csv.DictWriter(f, fieldnames=["name", "status", "is_active"])
-            writer.writeheader()
-            writer.writerows(processed_data)
+    to_csv(processed_data, filename="processed_data.csv", fieldnames=FIELD_NAMES_OUTPUT)
 
 
 if __name__ == "__main__":
