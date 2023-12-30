1012c18fa7c05a979400a3b75e3d9c6cf2b36bf5
Pano <panosevag@gmail.com>
2023-12-30 14:33:12 +0200

    completed Coupling and started Data

--- the_7_principles_of_modern_software_design/low_coupling/order_before.py
+++ the_7_principles_of_modern_software_design/low_coupling/order_before.py
@@ -15,27 +15,25 @@ class Order:
         self.prices: list[int] = []
         self.status: str = "open"
 
+    def add_item(self, name: str, quantity: int, price: int) -> None:
+        self.items.append(name)
+        self.quantities.append(quantity)
+        self.prices.append(price)
 
-def add_item(order: Order, name: str, quantity: int, price: int) -> None:
-    order.items.append(name)
-    order.quantities.append(quantity)
-    order.prices.append(price)
-
-
-def compute_total_price(order: Order) -> None:
-    total = 0
-    for i in range(len(order.prices)):
-        total += order.quantities[i] * order.prices[i]
-    print(f"The total price is: ${(total / 100):.2f}.")
+    def compute_total_price(self) -> None:
+        total = 0
+        for i in range(len(self.prices)):
+            total += self.quantities[i] * self.prices[i]
+        print(f"The total price is: ${(total / 100):.2f}.")
 
 
 def main() -> None:
     order = Order()
-    add_item(order, "Keyboard", 1, 5000)
-    add_item(order, "SSD", 1, 15000)
-    add_item(order, "USB cable", 2, 500)
+    order.add_item("Keyboard", 1, 5000)
+    order.add_item("SSD", 1, 15000)
+    order.add_item("USB cable", 2, 500)
 
-    compute_total_price(order)
+    order.compute_total_price()
 
 
 if __name__ == "__main__":
