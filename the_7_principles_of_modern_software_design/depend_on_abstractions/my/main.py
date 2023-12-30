0bbc02124884400aa1730bbbec36efffff4b839f
Pano <panosevag@gmail.com>
2023-12-31 00:33:35 +0200

    completed abstraction example

---
+++ the_7_principles_of_modern_software_design/depend_on_abstractions/my/main.py
@@ -0,0 +1,18 @@
+from pos.authorization import authorize_robot
+from pos.order import Order
+from pos.payment import DebitPaymentProcessor
+
+
+def main():
+    order = Order()
+    order.add_item("Keyboard", 1, 5000)
+    order.add_item("SSD", 1, 15000)
+    order.add_item("USB cable", 2, 500)
+
+    print(f"The total price is: ${(order.total_price / 100):.2f}.")
+    processor = DebitPaymentProcessor()
+    processor.pay(payable=order, authorize=authorize_robot)
+
+
+if __name__ == "__main__":
+    main()
