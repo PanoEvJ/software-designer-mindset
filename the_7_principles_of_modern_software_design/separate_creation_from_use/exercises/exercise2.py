f25148b9b30423e3e3fdb991136bee03836818be
Pano <panosevag@gmail.com>
2023-12-31 18:15:23 +0200

    exercise 2: completed

--- the_7_principles_of_modern_software_design/separate_creation_from_use/exercises/exercise2.py
+++ the_7_principles_of_modern_software_design/separate_creation_from_use/exercises/exercise2.py
@@ -1,6 +1,7 @@
 import random
 from dataclasses import dataclass
 from enum import StrEnum
+from typing import Protocol
 
 
 class EnemyType(StrEnum):
@@ -17,18 +18,76 @@ class Enemy:
     defense: int
 
 
-SPAWN_FACTORY: dict[str, Enemy] = {
-    "easy": Enemy(EnemyType.KNIGHT, health=100, attack_power=10, defense=5),
-    "medium": Enemy(EnemyType.KNIGHT, health=100, attack_power=10, defense=5),
-    "hard": Enemy(EnemyType.KNIGHT, health=100, attack_power=10, defense=5),
+class EnemyFactory(Protocol):
+    def spawn_enemy(self) -> list[Enemy]:
+        ...
+
+
+@dataclass
+class HardEnemy:
+    health = random.randint(70, 100)
+    attack_power = random.randint(70, 100)
+    defense = random.randint(70, 100)
+
+    def spawn_enemy(self) -> list[Enemy]:
+        return [
+            Enemy(
+                EnemyType.WIZARD,
+                health=self.health,
+                attack_power=self.attack_power,
+                defense=self.defense,
+            )
+        ]
+
+
+@dataclass
+class MediumEnemy:
+    health = random.randint(30, 70)
+    attack_power = random.randint(30, 70)
+    defense = random.randint(30, 70)
+
+    def spawn_enemy(self) -> list[Enemy]:
+        return [
+            Enemy(
+                enemy_type,
+                health=self.health,
+                attack_power=self.attack_power,
+                defense=self.defense,
+            )
+            for enemy_type in EnemyType
+        ]
+
+
+@dataclass
+class EasyEnemy:
+    health = random.randint(1, 30)
+    attack_power = random.randint(1, 30)
+    defense = random.randint(1, 30)
+
+    def spawn_enemy(self) -> list[Enemy]:
+        return [
+            Enemy(
+                enemy_type,
+                health=self.health,
+                attack_power=self.attack_power,
+                defense=self.defense,
+            )
+            for enemy_type in [EnemyType.KNIGHT, EnemyType.ARCHER]
+        ]
+
+
+FACTORY: dict[str, EnemyFactory] = {
+    "easy": EasyEnemy(),
+    "medium": MediumEnemy(),
+    "hard": HardEnemy(),
 }
 
 
 def read_choice():
     while True:
-        spawn_points = input(f"Enter desired difficulty ({', '.join(SPAWN_FACTORY)}):")
+        spawn_points = input(f"Enter desired difficulty ({', '.join(FACTORY)}):")
         try:
-            return SPAWN_FACTORY[spawn_points]
+            return FACTORY[spawn_points]
         except KeyError:
             print(f"Unknown difficulty level: {spawn_points}")
 
@@ -36,7 +95,8 @@ def read_choice():
 def main() -> None:
     enemies = read_choice()
     # enemy = Enemy(EnemyType.KNIGHT, health=100, attack_power=10, defense=5)
-    print(enemies)
+    new_enemies = enemies.spawn_enemy()
+    print(new_enemies)
 
 
 if __name__ == "__main__":
