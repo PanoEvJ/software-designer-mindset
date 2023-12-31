95b9dd19a84450d1c2e22cf6602b1b2b2b226bc1
Pano <panosevag@gmail.com>
2023-12-31 17:29:01 +0200

    exercise 2: 1st iteration

--- the_7_principles_of_modern_software_design/separate_creation_from_use/exercises/exercise2.py
+++ the_7_principles_of_modern_software_design/separate_creation_from_use/exercises/exercise2.py
@@ -1,3 +1,4 @@
+import random
 from dataclasses import dataclass
 from enum import StrEnum
 
@@ -16,9 +17,26 @@ class Enemy:
     defense: int
 
 
+SPAWN_FACTORY: dict[str, Enemy] = {
+    "easy": Enemy(EnemyType.KNIGHT, health=100, attack_power=10, defense=5),
+    "medium": Enemy(EnemyType.KNIGHT, health=100, attack_power=10, defense=5),
+    "hard": Enemy(EnemyType.KNIGHT, health=100, attack_power=10, defense=5),
+}
+
+
+def read_choice():
+    while True:
+        spawn_points = input(f"Enter desired difficulty ({', '.join(SPAWN_FACTORY)}):")
+        try:
+            return SPAWN_FACTORY[spawn_points]
+        except KeyError:
+            print(f"Unknown difficulty level: {spawn_points}")
+
+
 def main() -> None:
-    enemy = Enemy(EnemyType.KNIGHT, health=100, attack_power=10, defense=5)
-    print(enemy)
+    enemies = read_choice()
+    # enemy = Enemy(EnemyType.KNIGHT, health=100, attack_power=10, defense=5)
+    print(enemies)
 
 
 if __name__ == "__main__":
